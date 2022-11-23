d78444b95fe2fb7cfaabe27262343aec618833e4
madison2023 <78463010+madison2023@users.noreply.github.com>
2022-11-22 18:38:26 -0800

    displays midi note in database

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,6 +10,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var note;
+
+  //getNote() async {
+  final ref = FirebaseDatabase.instance.ref().child('notes/data');
+  //   final snapshot = await ref.child('notes/data').get();
+  //   if (snapshot.exists) {
+  //     print(snapshot.value);
+  //     setState(() {
+  //       note = snapshot.value;
+  //     });
+  //   } else {
+  //     print('No data available.');
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,21 +35,42 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: const Color.fromARGB(255, 155, 69, 170),
       ),
       body: Center(
-        child: SleekCircularSlider(
-          min: 0,
-          max: 50,
-          initialValue: 1,
-          appearance: CircularSliderAppearance(
-              infoProperties: InfoProperties(bottomLabelText: 'Volume')),
-          onChange: (double value) {
-            // callback providing a value while its being changed (with a pan gesture)
-          },
-          onChangeStart: (double startValue) {
-            // callback providing a starting value (when a pan gesture starts)
-          },
-          onChangeEnd: (double endValue) {
-            // ucallback providing an ending value (when a pan gesture ends)
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Midi Note", style: TextStyle(fontSize: 40)),
+            StreamBuilder(
+              stream: ref.onValue,
+              builder: ((context, snapshot) {
+                if (snapshot.hasData &&
+                    !snapshot.hasError &&
+                    snapshot.data?.snapshot.value != null) {
+                  return Text(
+                    snapshot.data!.snapshot.value.toString(),
+                    style: const TextStyle(fontSize: 50),
+                  );
+                }
+                return const CircularProgressIndicator();
+              }),
+            ),
+          ],
+
+          // child: SleekCircularSlider(
+          //   min: 0,
+          //   max: 50,
+          //   initialValue: 1,
+          //   appearance: CircularSliderAppearance(
+          //       infoProperties: InfoProperties(bottomLabelText: 'Volume')),
+          //   onChange: (double value) {
+          //     // callback providing a value while its being changed (with a pan gesture)
+          //   },
+          //   onChangeStart: (double startValue) {
+          //     // callback providing a starting value (when a pan gesture starts)
+          //   },
+          //   onChangeEnd: (double endValue) {
+          //     // ucallback providing an ending value (when a pan gesture ends)
+          //   },
+          // ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sleek_circular_slider/sleek_circular_slider.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:theremin_synth/homePage.dart';
 import 'firebase_options.dart';
